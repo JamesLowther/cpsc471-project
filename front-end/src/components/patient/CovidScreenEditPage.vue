9a46c5eac0ad8afc0165fd1eb45ea64da17a81e5
Logan Perry-Din <irfanster@hotmail.com>
2021-03-29 21:11:32 -0600

    changed back button to router(-1)

--- front-end/src/components/patient/CovidScreenEditPage.vue
+++ front-end/src/components/patient/CovidScreenEditPage.vue
@@ -1,13 +1,11 @@
 <template>
     <div id="report-edit">
         <div class="flex justify-end w-full fixed">
-            <router-link to="/patient-panel/forms">
-                <div
-                    class="text-white mt-5 shadow-lg transition duration-300 ease-in-out bg-gray-700 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 rounded-lg py-2 px-8 m-6"
-                >
+            <button class="justify-self-end" @click="$router.go(-1)">
+                <div class="text-white mt-5 shadow-lg transition duration-300 ease-in-out bg-gray-700 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 rounded-lg py-2 px-8 m-6">
                     Back
                 </div>
-            </router-link>
+            </button>
         </div>
         <div v-if="logged_in">
             <div class="flex flex-col">
