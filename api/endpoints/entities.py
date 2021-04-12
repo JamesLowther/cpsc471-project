c4b801fa4af45b47fcf6170dbd8a50c90ea701d6
brodyl <75949899+brodyl@users.noreply.github.com>
2021-04-12 02:24:14 -0600

    Changes to entities page
    Condensed sepeerate add/update posts into one
    Added delete abilities
    Improved response message
    
    GLITCH = if adding an entity with the same name, and deleteing it, when you add again the side effects are still there.....

--- api/endpoints/entities.py
+++ api/endpoints/entities.py
@@ -22,10 +22,11 @@ class EntitiesForms(Resource):
     parser.add_argument("entity_type", type=str, required=True, choices={"medication", "illness", "symptom"}, help="Bad choice: {error_msg}")
     parser.add_argument("method", type=str, required=True, choices={"add", "update", "delete", "query"}, help="Bad choice: {error_msg}")
     
-    parser.add_argument("med_name", type=str, required=False)
+
+    parser.add_argument("entity_name", type=str, required=False)
+    #Medication attributes
     parser.add_argument("is_pres", type=int, required=False)
-    
-    parser.add_argument("ill_name", type=str, required=False)
+    #illness attributes
     parser.add_argument("org_sys", type=str, required=False)
     
     # either side-effects or symptoms for newly added med or illness
@@ -65,18 +66,18 @@ class EntitiesForms(Resource):
         if args["method"] == "query":
             results = self.query(args)
 
-        # Only doctors and clerks can accses add/delete functions.
+        # Only doctors and clerks can access add/delete functions.
         elif current["user_type"] in {"doctor", "clerk"}:
             if args["method"] == "add":
                 return self.add_new_entity(args)
 
             elif args["method"] == "update":
-                # Write code to delete illness/medication.
+                # update illness/medication.
                 return self.update_entity(args)
 
             elif args["method"] == "delete":
-                # Write code to delete illness/medication.
-                pass
+                # delete illness/medication.
+                return self.delete_entity(args)
 
         return jsonify(
             logged_in=1,
@@ -151,18 +152,18 @@ class EntitiesForms(Resource):
         # catch any SQL errors eg key or unique name constraints
         if(args["entity_type"] == "medication"):
             try:
-                cursor.execute("INSERT INTO Medication VALUES (?, ?);", (args["med_name"], args["is_pres"]) )
+                cursor.execute("INSERT INTO Medication VALUES (?, ?);", (args["entity_name"], args["is_pres"]) )
 
                 # if users included side-effects, insert into database
                 if(args["effects"]):
                     for effect in args["effects"]:
-                        cursor.execute("INSERT INTO Side_Effects VALUES (?,?);", (args["med_name"], effect) )
+                        cursor.execute("INSERT INTO Side_Effects VALUES (?,?);", (args["entity_name"], effect) )
 
             except sqlite3.Error as er:
                 print(er)
                 con.close()
                 return jsonify(
-                    status=0,
+                    status=1,
                     error_msg=er,
                     sys_msg="Error: Could not Add",
                     )
@@ -170,13 +171,12 @@ class EntitiesForms(Resource):
         # Add a new illness, and its specified symptoms. Catch any SQL errors
         elif (args["entity_type"] == "illness"):
             try:
-                #cursor.execute("DELETE FROM Illness WHERE  (?);", (args["ill_name"],),)
-                cursor.execute("INSERT INTO Illness VALUES (?, ?);", (args["ill_name"], args["org_sys"],),)
+                cursor.execute("INSERT INTO Illness VALUES (?, ?);", (args["entity_name"], args["org_sys"],),)
 
                 # if users included symptoms, insert into database
                 if(args["effects"]):
                     for symptom in args["effects"]:
-                        cursor.execute("INSERT INTO Symptoms VALUES (?,?);", (args["ill_name"], symptom) )
+                        cursor.execute("INSERT INTO Symptoms VALUES (?,?);", (args["entity_name"], symptom) )
 
             except sqlite3.Error as er:
                 print(er)
@@ -208,14 +208,14 @@ class EntitiesForms(Resource):
         # catch any SQL errors eg key or unique name constraints
         if(args["entity_type"] == "medication"):
             try:
-                cursor.execute("DELETE FROM Medication WHERE Name = ?;", (args["med_name"],),)
-                cursor.execute("INSERT INTO Medication VALUES (?, ?);", (args["med_name"], args["is_pres"]) )
+                cursor.execute("DELETE FROM Medication WHERE Name = ?;", (args["entity_name"],),)
+                cursor.execute("INSERT INTO Medication VALUES (?, ?);", (args["entity_name"], args["is_pres"]) )
 
                 # if users included side-effects, insert into database
-                cursor.execute("DELETE FROM Side_Effects WHERE Med_Name = (?);", (args["med_name"],),)
+                cursor.execute("DELETE FROM Side_Effects WHERE Med_Name = (?);", (args["entity_name"],),)
                 if(args["effects"]):
                     for effect in args["effects"]:
-                        cursor.execute("INSERT INTO Side_Effects VALUES (?,?);", (args["med_name"], effect) )
+                        cursor.execute("INSERT INTO Side_Effects VALUES (?,?);", (args["entity_name"], effect) )
 
             except sqlite3.Error as er:
                 print(er)
@@ -229,14 +229,14 @@ class EntitiesForms(Resource):
         # Update an existing illness, and its specified symptoms. Catch any SQL errors
         elif (args["entity_type"] == "illness"):
             try:
-                cursor.execute("DELETE FROM Illness WHERE Name = ?;", (args["ill_name"],),)
-                cursor.execute("INSERT INTO Illness VALUES (?, ?);", (args["ill_name"], args["org_sys"],),)
+                cursor.execute("DELETE FROM Illness WHERE Name = ?;", (args["entity_name"],),)
+                cursor.execute("INSERT INTO Illness VALUES (?, ?);", (args["entity_name"], args["org_sys"],),)
 
                 # if users included symptoms, insert into database
-                cursor.execute("DELETE FROM Symptoms WHERE Illness_name = (?);", (args["ill_name"],),)
+                cursor.execute("DELETE FROM Symptoms WHERE Illness_name = (?);", (args["entity_name"],),)
                 if(args["effects"]):
                     for symptom in args["effects"]:
-                        cursor.execute("INSERT INTO Symptoms VALUES (?,?);", (args["ill_name"], symptom) )
+                        cursor.execute("INSERT INTO Symptoms VALUES (?,?);", (args["entity_name"], symptom) )
 
             except sqlite3.Error as er:
                 print(er)
@@ -254,4 +254,50 @@ class EntitiesForms(Resource):
         return jsonify(
             status=1,
             sys_msg="Successfully Updated",
+            )
+
+    def delete_entity(self, args):
+        """
+        Update an existing entity in the database and its corresponding
+        effects or symptoms.
+        """
+
+        con, cursor = db.connect_db()
+
+        # Update an existing medication, and its specified side-effects
+        # catch any SQL errors eg key or unique name constraints
+        if(args["entity_type"] == "medication"):
+            try:
+                cursor.execute("DELETE FROM Medication WHERE Name = ?;", (args["entity_name"],),)
+
+            except sqlite3.Error as er:
+                print(er)
+                con.close()
+                return jsonify(
+                    status=0,
+                    error_msg=er,
+                    sys_msg="Error: Could not delete",
+                    )
+        
+        # Update an existing illness, and its specified symptoms. Catch any SQL errors
+        elif (args["entity_type"] == "illness"):
+            try:
+                cursor.execute("DELETE FROM Illness WHERE Name = ?;", (args["entity_name"],),)
+
+            except sqlite3.Error as er:
+                print(er)
+                con.close()
+                return jsonify(
+                    status=0,
+                    error_msg=er,
+                    sys_msg="Error: Could not delete",
+                    )
+
+
+        con.commit()
+        con.close()
+
+        return jsonify(
+            status=1,
+            sys_msg="Successfully Deleted",
             )
