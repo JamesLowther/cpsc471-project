807b33612f9a7b89bb2977a6d7eb6bb212f7f790
brodyl <75949899+brodyl@users.noreply.github.com>
2021-04-16 13:11:29 -0600

    Setting default values for TPAL on med_history

--- front-end/src/components/doctor/DoctorFormPage.vue
+++ front-end/src/components/doctor/DoctorFormPage.vue
@@ -14,6 +14,10 @@
                 <p class="text-3xl md:text-4xl lg:text-5xl mt-20 bg-blue-300">Doctor Panel</p>
                 <p class="text-2xl md:text-3xl lg:text-4xl mt-4 bg-blue-300">Patient Reports</p>
             </div>
+            
+
+
+
             <div class="flex flex-col items-center pt-10 w-full mt-16 mb-4">
                 <table class="w-full ml-auto md: mr-auto md:w-3/4 2xl:w-1/2 mb-3 rounded-b-none shadow-lg">
                     <thead>
@@ -72,8 +76,143 @@
             <p class="text-5xl mt-20">Forbidden</p>
             <p class="text-3xl mt-4">You're not logged in</p>
         </div>
+
+
+
+
+                <!-- <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5"> -->
+        <div class="flex flex-col items-center pt-10 w-full mt-16 mb-4">
+                <table class="w-full flex flex-row ml-auto md: mr-auto md:w-3/4 2xl:w-1/2 mb-3 shadow-lg">
+                    <thead>
+                        <tr class="bg-red-400 flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0"
+                            v-for="(patient, i) in patientList"
+                            :key="`${i}-${i}`">
+                            <th class="text-lg text-white w-full bg-red-500 border-teal border-b-2 border-r-2"> 
+                                Name
+                            </th>
+                            <th class="text-lg text-white bg-blue-500 border-b-2 p-2 pb-0">
+                                Report-#
+                            </th>
+                            <th class="text-lg text-white bg-blue-500 rounded-tr-2xl border-teal border-b-2 border-l-2 px-8">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="flex-1 sm:flex-none">
+                        <tr class="bg-blue-100 flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0"
+                            v-for="(patient, i) in patientList"
+                            :key="`${i}-${patient}`"
+                        >
+                            <td class="border-blue-300 border-2">
+                                {{ patient.Lname }}, {{ patient.Fname }}
+                            </td>
+                            <td class="border-blue-300 border-2">
+                                {{ patient.Report_ID }}
+                            </td>
+                            <td class="border-blue-300 border-2">
+                                <router-link
+                                    :to="{
+                                        name: 'doctor-edit-report',
+                                        params: { p_ssn: patient.P_SSN, id: patient.Report_ID },
+                                    }"
+                                >
+                                    <div
+                                        class="text-white my-2 shadow-lg transition duration-300 ease-in-out bg-gray-700 hover:bg-blue-600 rounded-lg py-1 px-4 mx-6 my-1"
+                                    >
+                                        View
+                                    </div>
+                                </router-link>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+<div class="container">
+        <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+            <thead class="text-white">
+                <tr class="flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th class="bg-teal-400 p-3 text-left">Name</th>
+                    <th class="bg-teal-400 p-3 text-left">Email</th>
+                    <th class="bg-teal-400 p-3 text-left" width="110px">Actions</th>
+                </tr>
+                <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th class="p-3 text-left">Name</th>
+                    <th class="p-3 text-left">Email</th>
+                    <th class="p-3 text-left" width="110px">Actions</th>
+                </tr>
+                <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                  <th class="p-3 text-left">Name</th>
+                  <th class="p-3 text-left">Email</th>
+                  <th class="p-3 text-left" width="110px">Actions</th>
+              </tr>
+                <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                  <th class="p-3 text-left">Name</th>
+                  <th class="p-3 text-left">Email</th>
+                  <th class="p-3 text-left" width="110px">Actions</th>
+              </tr>
+            </thead>
+            <tbody class="flex-1 sm:flex-none">
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">John Covv</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">contato@johncovv.com</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">Michael Jackson</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">m_jackson@mail.com</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">Julia</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">julia@mail.com</td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                  <td class="border-grey-light border hover:bg-gray-100 p-3">Martin Madrazo</td>
+                  <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">martin.madrazo@mail.com</td>
+                  <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+
     </div>
 </template>
+
+<style>
+  html,
+  body {
+    height: 100%;
+  }
+
+  @media (min-width: 640px) {
+    table {
+      display: inline-table !important;
+    }
+
+    thead tr:not(:first-child) {
+      display: none;
+    }
+  }
+
+  td:not(:last-child) {
+        background-color: rgb(52, 144, 220, .4);
+    border-bottom: 0;
+  }
+
+  th:not(:last-child) {
+        background-color: rgb(52, 144, 220);
+    border-bottom: 2px solid rgba(0, 0, 0, .1);
+  }
+</style>
+
 
 <script>
 import axios from "axios";
