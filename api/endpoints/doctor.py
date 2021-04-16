1214ec66e7514c3f97645357ac66452b6332ce3b
James Lowther <jamesevlowther@gmail.com>
2021-04-16 16:05:33 -0600

    Improved comments and formatting for python API

--- api/endpoints/doctor.py
+++ api/endpoints/doctor.py
@@ -10,13 +10,16 @@ import json
 
 class Doctor(Resource):
     def get(self):
+        """Handle get request for doctor endpoint.
+        """
+
+        # Returns doctor information for logged in user.
         if verify_jwt_in_request():
             current = get_jwt_identity()
             if current["user_type"] == "doctor":
                 return jsonify({**current, "logged_in": 1})
             else:
                 return jsonify({**current, "logged_in": 0})
-
 
 
 class DoctorForms(Resource):
@@ -37,9 +40,10 @@ class DoctorForms(Resource):
     parser.add_argument("medCenter_to_add", type=str)
     parser.add_argument("complaint", type=str)
 
-
     def get(self):
-        
+        """Handle get request for doctor/forms endpoint.
+        """
+
         if verify_jwt_in_request():
             current = get_jwt_identity()
 
@@ -57,11 +61,12 @@ class DoctorForms(Resource):
             patients=[dict(x) for x in patients]
         )
 
-
     def post(self):
-        
+        """Handle post request for doctor/forms endpoint.
+        """
+
         args = self.parser.parse_args()
-        
+
         if args["thePatientName"] is not None:
             if(args["p_ssn"] == -1):
 
@@ -69,7 +74,8 @@ class DoctorForms(Resource):
 
                 # Get form.
                 cursor.execute(
-                    "SELECT * FROM New_Applicant_Form AS a LEFT OUTER JOIN  Report AS r ON r.P_SSN = a.P_SSN WHERE r.Report_ID NOT NULL AND Fname ||' '|| Lname  LIKE ? ORDER BY Fname ASC, Report_ID DESC LIMIT 10;", ("%" + args["thePatientName"] + "%",)
+                    "SELECT * FROM New_Applicant_Form AS a LEFT OUTER JOIN  Report AS r ON r.P_SSN = a.P_SSN WHERE r.Report_ID NOT NULL AND Fname ||' '|| Lname  LIKE ? ORDER BY Fname ASC, Report_ID DESC LIMIT 10;", (
+                        "%" + args["thePatientName"] + "%",)
                 )
                 patients = cursor.fetchall()
 
@@ -81,16 +87,17 @@ class DoctorForms(Resource):
                 )
 
         elif (args["p_ssn"] != -1):
-            #If the doctor wants to save changes to the complaint form:
+            # If the doctor wants to save changes to the complaint form.
             if (args["action_type"] == "update_complaint"):
-                self.update_complaint(args["p_ssn"], args["id"], args["complaint"])
+                self.update_complaint(
+                    args["p_ssn"], args["id"], args["complaint"])
 
                 return jsonify(
                     logged_in=1,
                 )
 
             elif (args["action_type"] == "get_form"):
-                if (args["form_type"] == "report"): 
+                if (args["form_type"] == "report"):
 
                     form = self.get_report(args["p_ssn"], args["id"])
 
@@ -100,144 +107,168 @@ class DoctorForms(Resource):
                         form=form,
                     )
 
-            #If the doctor is requesting to remove a Diagnosised Illness from the patients report
+            # If the doctor is requesting to remove a Diagnosised Illness from the patients report.
             elif (args["action_type"] == "remove_illness"):
 
-                    self.remove_diagnosis(args["p_ssn"], args["id"], args["illness_to_remove"])
+                self.remove_diagnosis(
+                    args["p_ssn"], args["id"], args["illness_to_remove"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
 
-            #If the doctor is requesting to add a Diagnosised Illness from the patients report
+            # If the doctor is requesting to add a Diagnosised Illness from the patients report.
             elif (args["action_type"] == "add_illness"):
 
-                    self.add_diagnosis(args["p_ssn"], args["id"], args["illness_to_add"])
+                self.add_diagnosis(
+                    args["p_ssn"], args["id"], args["illness_to_add"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
 
-            #If the doctor is requesting to remove a Medication from the patients report
+            # If the doctor is requesting to remove a Medication from the patients report.
             elif (args["action_type"] == "remove_medication"):
 
-                    self.remove_medication(args["p_ssn"], args["id"], args["medication_to_remove"])
+                self.remove_medication(
+                    args["p_ssn"], args["id"], args["medication_to_remove"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
 
-            #If the doctor is requesting to add a Medication from the patients report
+            # If the doctor is requesting to add a Medication from the patients report.
             elif (args["action_type"] == "add_medication"):
 
-                    self.add_medication(args["p_ssn"], args["id"], args["medication_to_add"])
+                self.add_medication(
+                    args["p_ssn"], args["id"], args["medication_to_add"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
-            #If the doctor is requesting to remove a Medical Center from the patients report
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
+            # If the doctor is requesting to remove a Medical Center from the patients report.
             elif (args["action_type"] == "remove_medCenter"):
 
-                    self.remove_medCenter(args["p_ssn"], args["id"], args["medCenter_to_remove"])
+                self.remove_medCenter(
+                    args["p_ssn"], args["id"], args["medCenter_to_remove"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
 
-            #If the doctor is requesting to add a Medical Center from the patients report
+            # If the doctor is requesting to add a Medical Center from the patients report.
             elif (args["action_type"] == "add_medCenter"):
 
-                    self.add_medCenter(args["p_ssn"], args["id"], args["medCenter_to_add"])
+                self.add_medCenter(
+                    args["p_ssn"], args["id"], args["medCenter_to_add"])
 
-                    return jsonify(
-                        logged_in=1,
-                        p_ssn=args["p_ssn"],
-                    )
+                return jsonify(
+                    logged_in=1,
+                    p_ssn=args["p_ssn"],
+                )
 
     def update_complaint(self, ssn, id, complaint):
+        """Update the compaint for a patient's report.
+        """
 
         con, cursor = db.connect_db()
 
         # Update the patients complaint for the report
-        cursor.execute("UPDATE Report SET Complaint = ? WHERE P_SSN = ? AND Report_ID = ?;", (complaint, ssn, id,),)
+        cursor.execute(
+            "UPDATE Report SET Complaint = ? WHERE P_SSN = ? AND Report_ID = ?;", (complaint, ssn, id,),)
         con.commit()
         con.close()
 
-
     def add_diagnosis(self, ssn, id, illness):
+        """Add a diagnosis to a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Add a diagnosed illness.
-        cursor.execute("INSERT INTO Diagnoses (Illness_Name, Report_ID, P_SSN) VALUES (?,?,?);", (illness, id, ssn,),)
+        cursor.execute(
+            "INSERT INTO Diagnoses (Illness_Name, Report_ID, P_SSN) VALUES (?,?,?);", (illness, id, ssn,),)
 
         con.commit()
         con.close()
 
-
     def remove_diagnosis(self, ssn, id, illness):
+        """Remove a diagnosis from a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Remove a diagnosed illness from patients report.
-        cursor.execute("DELETE FROM Diagnoses WHERE P_SSN = ? AND Report_ID = ? AND Illness_name = ?;", (ssn, id, illness,),)
+        cursor.execute(
+            "DELETE FROM Diagnoses WHERE P_SSN = ? AND Report_ID = ? AND Illness_name = ?;", (ssn, id, illness,),)
 
         con.commit()
         con.close()
 
     def add_medication(self, ssn, id, medication):
+        """Add a medication for to a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Add a medication to patients report.
-        cursor.execute("INSERT INTO Prescribes (Med_Name, Report_ID, P_SSN) VALUES (?,?,?);", (medication, id, ssn,),)
+        cursor.execute(
+            "INSERT INTO Prescribes (Med_Name, Report_ID, P_SSN) VALUES (?,?,?);", (medication, id, ssn,),)
 
         con.commit()
         con.close()
 
-
     def remove_medication(self, ssn, id, medication):
+        """Remove a medication from a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Remove a medication that was prescribed on patients report.
-        cursor.execute("DELETE FROM Prescribes WHERE P_SSN = ? AND Report_ID = ? AND Med_Name = ?;", (ssn, id, medication,),)
+        cursor.execute(
+            "DELETE FROM Prescribes WHERE P_SSN = ? AND Report_ID = ? AND Med_Name = ?;", (ssn, id, medication,),)
 
         con.commit()
         con.close()
 
     def add_medCenter(self, ssn, id, medCenter):
+        """Add a medical centre to a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Add a medcenter to patients report.
-        cursor.execute("INSERT INTO Assigned (Report_ID, P_SSN, MedCenter_Name) VALUES (?,?,?);", (id, ssn, medCenter,),)
+        cursor.execute(
+            "INSERT INTO Assigned (Report_ID, P_SSN, MedCenter_Name) VALUES (?,?,?);", (id, ssn, medCenter,),)
 
         con.commit()
         con.close()
 
-
     def remove_medCenter(self, ssn, id, medCenter):
+        """Remove a medical centr from a report.
+        """
 
         con, cursor = db.connect_db()
 
         # Remove a medcenter from patients report.
-        cursor.execute("DELETE FROM Assigned WHERE P_SSN = ? AND Report_ID = ? AND MedCenter_Name = ?;", (ssn, id, medCenter,),)
+        cursor.execute(
+            "DELETE FROM Assigned WHERE P_SSN = ? AND Report_ID = ? AND MedCenter_Name = ?;", (ssn, id, medCenter,),)
 
         con.commit()
         con.close()
 
-
     def get_report(self, ssn, id):
+        """Get the data from a patient's report.
+        """
 
         con, cursor = db.connect_db()
 
-        #Get patient name.
+        # Get patient name.
         cursor.execute(
             "SELECT a.Fname, a.Initial, a.Lname, a.Gender, a.Sex, CAST(STRFTIME('%Y.%m%d', 'now') - STRFTIME('%Y.%m%d', a.DoB) AS INT) AS Age FROM New_Applicant_Form AS a WHERE P_SSN = ?;", (ssn,),
         )
@@ -301,29 +332,30 @@ class DoctorForms(Resource):
 
         # Check if patient has a medical history filled out
 
-        # initialize dict() variables in case the if statement can't
+        # Initialize dict() variables in case the if statement can't.
         TPAL = {}
         past_illnesses = {}
         allergies = {}
         immunizations = {}
 
-        #to check is theres a medical history tuple
-        has_row = 0;
-        cursor.execute("SELECT * FROM Medical_History WHERE P_SSN = ?;", (ssn,))
-        #has_row will be true if a medical history tuple exists for the patient
+        # To check is theres a medical history tuple.
+        has_row = 0
+        cursor.execute(
+            "SELECT * FROM Medical_History WHERE P_SSN = ?;", (ssn,))
+        # has_row will be true if a medical history tuple exists for the patient
         has_row = cursor.fetchone()
-        
-        #if history exists:
+
+        # If history exists:
         # retrieve TPAL (Pregnancy) info
         if has_row:
-            # retrieve TPAL info
+            # Retrieve TPAL info.
             cursor.execute(
                 "SELECT TPAL_total, TPAL_preterm, TPAL_aborted, TPAL_living FROM Medical_History WHERE P_SSN = ?;",
                 (ssn,)
             )
             TPAL = cursor.fetchone()
 
-            # retrieve Past_Illnesses
+            # Retrieve Past_Illnesses.
             cursor.execute(
                 "SELECT Illness_name, Age_of_onset FROM Past_Illnesses WHERE P_SSN = ?;",
                 (ssn,)
@@ -331,7 +363,7 @@ class DoctorForms(Resource):
             past_illnesses = cursor.fetchall()
             past_illnesses = [dict(x) for x in past_illnesses]
 
-            # retrieve Allergies
+            # Retrieve Allergies.
             cursor.execute(
                 "SELECT Allergy FROM Allergies WHERE P_SSN = ?;",
                 (ssn,)
@@ -339,7 +371,7 @@ class DoctorForms(Resource):
             allergies = cursor.fetchall()
             allergies = [dict(x) for x in allergies]
 
-            # retrieve Immunizations
+            # Retrieve Immunizations.
             cursor.execute(
                 "SELECT Immunization FROM Immunization WHERE P_SSN = ?;",
                 (ssn,)
@@ -347,13 +379,12 @@ class DoctorForms(Resource):
             immunizations = cursor.fetchall()
             immunizations = [dict(x) for x in immunizations]
 
-        # retrieve If they've passed their covid screen
+        # Retrieve if they've passed their covid screen.
         cursor.execute(
             "SELECT Has_passed, MAX(Date) FROM Covid_Screen WHERE P_SSN = ?;",
             (ssn,)
         )
         covid_screen = cursor.fetchone()
-
 
         con.close()
 
@@ -370,4 +401,4 @@ class DoctorForms(Resource):
             "allergies": list(allergies),
             "immunizations": list(immunizations),
             **dict(covid_screen),
-            }
+        }
